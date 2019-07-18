89c3201104f5173a58c6eb337b36663dc8bfa3ec
Ben Ellis <ben@niftlabs.io>
2019-07-18 14:21:53 -0700

    Add CSS color, add build dir

---
+++ build/precache-manifest.ef7027b72489d7c070fd0b4e814a27a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fe661ab31c3f60d20689568e81877a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "993ec3868dda733e10a5",
+    "url": "/static/css/main.211f39f9.chunk.css"
+  },
+  {
+    "revision": "88a73f12759f096ea0c8",
+    "url": "/static/js/2.223a052c.chunk.js"
+  },
+  {
+    "revision": "993ec3868dda733e10a5",
+    "url": "/static/js/main.7ede1f6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c108b9e34110f973e5fa39a4ae103fb5",
+    "url": "/static/media/crop.c108b9e3.jpg"
+  }
+]);
